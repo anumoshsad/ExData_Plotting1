364d3bd2157eb5d53823b3069c9ef60a46b6eb2c
Shouman <anumoshsad@gmail.com>
2016-01-07 23:19:00 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,11 @@
+#assuming that you have downloaded the txt file in your working directory
 d <- read.csv2("household_power_consumption.txt", dec = ".",stringsAsFactors=FALSE);
 
 dat <- d[d$Date == "1/2/2007" | d$Date == "2/2/2007",];
 dtime <- paste(dat$Date, dat$Time);
 dt <- strptime(dtime, format = "%d/%m/%Y %H:%M:%S");
 
+# here ? will automatically turn into NA, you will get warning though.
 for(i in 3:9){
     dat[,i] = as.numeric(dat[,i])
 }
